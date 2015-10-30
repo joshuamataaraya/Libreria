a44fa45a31d0f91eca72ecdd63955e69393c1e75
xelop <adrianlq8@hotmail.com>
2015-10-30 11:58:28 -0600

    nuevos web services

--- app/WebService/getCategorias.php
+++ app/WebService/getCategorias.php
@@ -3,14 +3,17 @@
 ini_set('display_errors', 1);
 $conn = mysql_connect("localhost", "root", "Lopezx01");
 mysql_select_db('mnemonic', $conn);
+
+$Name = $_GET["name"];
+$Name2     = mysql_real_escape_string($Name);
 //call query
-$qur = mysql_query("select p.nombre, p.precio, p.precioOferta, p.imagen from productos p where p.esOferta = true");
+$qur = mysql_query("select p.nombre, p.precio, p.imagen from productos p where p.categoria = '$Name2' ");
 
 $result = array();
 while($r = mysql_fetch_array($qur)){
   extract($r);
   
-  $result[] = array("nombre" => $nombre, "precio" => $precio, "precioOferta" => $precioOferta, "imagen" => $imagen);
+  $result[] = array("nombre" => $nombre, "precio" => $precio, "imagen" => $imagen);
 }
 @mysql_close($conn);
 /*Output*/
